9f32faf729b63ec29744dbf3b083500c8a171ab6
Abhinav Raghunandan <93826081+Abhinav158@users.noreply.github.com>
2022-02-23 20:02:05 +0530

    Update training.py

--- training.py
+++ training.py
@@ -4,7 +4,7 @@ from self_driving_car import game
 import random
 
 NUM_INPUT = 3
-episilon = 1
+epsilon = 1
 gamma = 0.9
 
 
@@ -23,8 +23,42 @@ def deep_q_train(model, params):
     # first observe then train
     while t < training_frames:
         t += 1
-        # let the game play for a while to learn about the environmnent
+        #reset weights of neural network
+        model.compile(loss='mse', optimizer=rms)  
+
+        _,state = game_state.frame_step((2))
+         #initial state of the car 
+        
+        
+        #All possible actions from that state
+        qval = model.predict(state.reshape(1,num_sensors), batch_size=1)
+
+        if (random.random() < epsilon):
+            action = np.random.randint(0,4) 
+         #take random action choice: 0 - up, 1 - down, 2 - left, 3 - right
+        else:
+            #Take maximum value of the Q values
+            action = (np.argmax(qval))
+
+        #Take action, observe new state S' and record it
+        reward, new_state = game_state.frame_step(state, action)
+
+        #Experience replay storage
+        if (len(replay) < buffer): 
+           replay.append((state, action, reward, new_state))
+        else: #if buffer full, overwrite old values
+            if (h < (buffer-1)):
+                h += 1
+            else:
+                h = 0
+
+#Initially, we want the car to take random actions to familiarize itself with the environment 
+#We use the neural network only after a certain number of frames, in this case "observe" number of frames
+#Decrement epsilon only after "observe" number of frames to eventually take the help of neural network
         if (t > observe):
+            decrement = 1/train_frames
+            if epsilon > 0.1: #We set the lower cap for epsilon as 0.1
+                epsilon -= decrement
             print("Game #: %s" % (model.i,))
             minibatch = random.sample(replay, batch_size)
             X_train, y_train = minibatch_process(model, minibatch)
