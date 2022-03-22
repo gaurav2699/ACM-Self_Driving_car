0cf2633d19f4070dd0b2276dc5d08bfc1d38e521
gaurav2699 <gauravsingh2699@gmail.com>
2022-03-22 14:28:22 +0530

    added playing file

---
+++ playing.py
@@ -0,0 +1,14 @@
+from self_driving_car import game
+import numpy as np
+from neural_network import neural_network
+
+NUM_SENSORS = 3
+
+def play(model):
+    
+
+
+if __name__ == "__main__":
+    saved_model = 'saved-models/164-150-100-50000-250.h5'
+    model = neural_net(NUM_SENSORS, [164, 150], saved_model)
+    play(model)
