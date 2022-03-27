dbbfffdfc13d7b5eec4c5aa9b9a34172734f0808
gaurav2699 <gauravsingh2699@gmail.com>
2022-03-27 15:02:04 +0530

    Added demoing part

--- neural_network.py
+++ neural_network.py
@@ -3,7 +3,7 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Activation
 from tensorflow.keras.optimizers import RMSprop
 
-def neural_network(num_sensors, params):
+def neural_network(num_sensors, params, load=False):
     model = Sequential()
     model.add(Dense(params[0], input_shape=(num_sensors,), activation='relu'))
     model.add(Dropout(0.2))
@@ -12,5 +12,7 @@ def neural_network(num_sensors, params):
     model.add(Dense(3, activation='linear'))
     rms = RMSprop()
     model.compile(loss='mse', optimizer=rms)
+    if load:
+        model.load_weights(load)
     return model
 
--- playing.py
+++ playing.py
@@ -4,11 +4,25 @@ from neural_network import neural_network
 
 NUM_SENSORS = 3
 
+
 def play(model):
-    
+    game_demo = game.Game()
+    _,state = game_demo.frame_step((2)) #action is taken as 2
+    status = 1
+    print("here")
+    # while game still in progress
+    while status == 1:
+        
+        qval = model.predict(state, batch_size=1)
+        print(qval)
+        action = (np.argmax(qval))  # take action with highest Q-value
+        _,state = game_demo.frame_step(action)
+        # x, y = game_demo.car_body.position
+        # reward = game.Game.get_sonar_readings(x, y, game.Game.car_body.angle)
+
 
 
 if __name__ == "__main__":
-    saved_model = 'saved-models/164-150-100-50000-250.h5'
-    model = neural_net(NUM_SENSORS, [164, 150], saved_model)
+    saved_model = 'saved_models/164-150-100-50000-250.h5'
+    model = neural_network(NUM_SENSORS, [164, 150], saved_model)
     play(model)
